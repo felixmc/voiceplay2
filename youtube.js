75a38e9a069e361f8b2a54204851e87d53fbf573
Felix Milea-Ciobanu <felixmilea@gmail.com>
2017-02-06 01:46:19 -0700

    fixes

--- youtube.js
+++ youtube.js
@@ -12,14 +12,14 @@ exports.intents = {
         const item = result.items[0]
         console.log('preparing video..', item)
         ytdl('https://www.youtube.com/watch?v=' + item.id.videoId,
-//            { quality: 'highest', filter: format => format.container === 'mp4' },
+           { quality: 'highest', filter: format => format.container === 'mp4' },
             (err, info) => {
               console.log('download response:', err, info)
               if (err || !info || !info.formats.length) {
-                callback({ text: `Trouble playing YouTube video, please try again later.`, shouldEndSession: true })
+                cb({ text: `Trouble playing YouTube video, please try again later.`, shouldEndSession: true })
               }  else {
                 const video = info.formats[0]
-                callback({ text: `Playing "${item.snippet.title}" from YouTube`, shouldEndSession: true, mediaUrl: video.url })
+                cb({ text: `Playing "${item.snippet.title}" from YouTube`, shouldEndSession: true, mediaUrl: video.url })
               }
             })
       } else {
