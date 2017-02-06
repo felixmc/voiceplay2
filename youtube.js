2d3691fb73315ca01f546319c8cb0336c86a81b1
Felix Milea-Ciobanu <felixmilea@gmail.com>
2017-02-06 02:05:51 -0700

    debugging

--- youtube.js
+++ youtube.js
@@ -12,17 +12,17 @@ exports.intents = {
         const item = result.items[0]
         const url = 'https://192.168.1.135/youtube/' + item.id.videoId
         console.log('preparing video..', item, 'https://www.youtube.com/watch?v=' + item.id.videoId)
-        cb({ text: `Playing "${item.snippet.title}" from YouTube`, shouldEndSession: true, mediaUrl: url })
-        // ytdl.getInfo('https://www.youtube.com/watch?v=' + item.id.videoId,
-        //    { quality: 'highest', filter: format => format.container === 'mp4' },
-        //     (err, info) => {
-        //       if (err || !info || !info.formats.length) {
-        //         cb({ text: `Trouble playing YouTube video, please try again later.`, shouldEndSession: true })
-        //       }  else {
-        //         const video = info.formats[0]
-        //         cb({ text: `Playing "${item.snippet.title}" from YouTube`, shouldEndSession: true, mediaUrl: video.url })
-        //       }
-        //     })
+        // cb({ text: `Playing "${item.snippet.title}" from YouTube`, shouldEndSession: true, mediaUrl: url })
+        ytdl.getInfo('https://www.youtube.com/watch?v=' + item.id.videoId,
+           { quality: 'highest', filter: format => format.container === 'mp4' },
+            (err, info) => {
+              if (err || !info || !info.formats.length) {
+                cb({ text: `Trouble playing YouTube video, please try again later.`, shouldEndSession: true })
+              }  else {
+                const video = info.formats[0]
+                cb({ text: `Playing "${item.snippet.title}" from YouTube`, shouldEndSession: true, mediaUrl: decodeURIComponent(video.url) })
+              }
+            })
       } else {
         cb({ text: 'I could not find any results. Play something else?', shouldEndSession: false })
       }
