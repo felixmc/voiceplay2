4017df63af4bf2a9d008f7b5afce4f8a0c62fdc6
Felix Milea-Ciobanu <felixmilea@gmail.com>
2017-02-06 01:35:50 -0700

    tweaks

--- parser.js
+++ parser.js
@@ -2,6 +2,7 @@ const intents = [
   require('./youtube'),
 ].reduce((intents, app) =>  {
   intents[app.name] = app.intents
+  return intents
 }, {})
 
 exports.parseIntent = (intent, cb) => {
@@ -9,6 +10,7 @@ exports.parseIntent = (intent, cb) => {
   const appInt = intents[0]
   const actionInt = intents[1]
 
+  console.log(intents)
   console.log('parsing intent:', appInt, actionInt, intent)
 
   if (intents[appInt] && intents[appInt][actionInt]) {
